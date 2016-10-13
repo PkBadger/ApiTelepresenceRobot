539dfb911bcc7d9b76527ec45056f4c9fb4b8989
Alex Perez <aleexpeerez@gmail.com>
2016-10-13 11:08:37 -0600

    added i2c communication

--- client/ImplementClient.py
+++ client/ImplementClient.py
@@ -1,5 +1,22 @@
 from client import GetData
 import RPi.GPIO as GPIO
+ # -*- coding: utf-8 -*-
+import time             #libreria time para delays
+from smbus2 import SMBus
+import smbus2
+import subprocess
+#from __main__ import *
+arduino = 0x08              #se define la dirección del esclavo
+
+def writeBlock(address,register,data):
+        flag=1
+        while(flag==1):
+                try:
+                        bus.write_i2c_block_data(address,register,data) #Data is list of bytes
+                        flag=0
+                except IOError:
+                        flag=1
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.OUT)
@@ -16,6 +33,10 @@ pwmY.ChangeDutyCycle(13)
 
 def callbackSpeedDir(motorL,motorR,dirL,dirR):
     print "speed "+motorR +" "+ motorL + " " + dirL + " "+ dirR
+    data = [motorR,motorL]
+    writeBlock(arduino,0,data)
+
+
 def callbackCamera(x,y):
     print "x= "+x+" y= "+y
 
